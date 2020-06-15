8e25e72bd6759d6912af6e491bfabcdfe9ac56c3
nolongtail <nameyee@gmail.com>
2020-06-15 22:28:20 +0800

    Major Changes:  refactor both file

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.core.text import LabelBase
 from kivy.utils import get_color_from_hex
 from kivy.clock import Clock
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager,Screen
-from kivy.garden.navigationdrawer import NavigationDrawer
 from kivy.animation import Animation
-from kivy.properties import BooleanProperty, NumericProperty, StringProperty
+from kivy.properties import BooleanProperty, StringProperty, ListProperty
 from kivy.uix.behaviors import ButtonBehavior
 
 import serial
 from datetime import datetime
 
-
-class Navigator(NavigationDrawer):
-    pass
+# Constant
+COLOR = {'red': (1,0,0,1), 
+        'yellow': (1,1,0,1), 
+        'green': (0,1,0,1), 
+        'standby': (1,0,0,1) }
 
 class Counter(Label):
-    pass
-
-class MainScreen(Screen):
-    pass
-
-class NavScreen(Screen):
-    pass
-
-class ScreenManagement(ScreenManager):
     pass
 
 class SelBox(ButtonBehavior, BoxLayout):
@@ -40,47 +28,37 @@ class DashApp(App):
     '''Root Class'''
     # property of traffic light counter
     connect = BooleanProperty(False)
-    state = StringProperty('')
-    timeleft = NumericProperty(0)
+    state = ListProperty((1,1,1,1))
+    timeleft = StringProperty('Counter')
+    timenow = StringProperty('04:58:00')
 
-    def nav(self):
-        # self.root.ids.navbox.pos_hint = {'right':1}
-        # print(self.root.children[0].ids)
-        self.root.children[0].ids.nav.toggle_state()
+    def nav(self, obj, touch, nav):
+        if obj.collide_point(*touch.pos):
+            nav.toggle_state()
+            return True # stop propagating event
+        return False
 
-    def sleep(self):
-        self.nav()
-        Clock.unschedule(self.curr_time)
-        self.root.current = 'second'
+    def sleep(self, obj, touch, nav):
+        if obj.collide_point(*touch.pos):
+            nav.toggle_state()
+            Clock.unschedule(self.curr_time)
+            self.root.current = 'second'
+            return True # stop propagating event
+        return False
 
-    def wake(self):
-        self.root.current = 'main'
-        self.curr_time = Clock.schedule_interval(self.update_time, 1)
-        
+
+    def wake(self, obj, touch):
+        if obj.collide_point(*touch.pos):
+            self.root.current = 'main'
+            self.curr_time = Clock.schedule_interval(self.update_time, 1)
+            return True # stop propagating event
+        return False
 
     def add_counter(self,dt):
-        self.root.children[0].ids.mainpanel.add_widget(self.wid,index=1)
+        self.root.children[0].ids["mainpanel"].add_widget(self.wid,index=1)
     
     def remove_counter(self, dt):
-        self.root.children[0].ids.mainpanel.remove_widget(self.wid)
-
-    def on_state(self, obj, state):
-        if self.connect == True:
-            if state == 'red':
-                self.wid.color = (1,0,0,1)
-            elif state == 'yellow':
-                self.wid.color = (1,1,0,1)
-            elif state == 'green':
-                self.wid.color = (0,1,0,1)
-            elif state == 'standby':
-                self.wid.color = (1,0,0,1)
-            else:
-                self.wid.color = (1,1,1,1)
-
-    def on_timeleft(self, obj, value):
-        '''handle the time left of the traffic light'''
-        if self.connect == True:
-            self.wid.text = str(value)
+        self.root.children[0].ids["mainpanel"].remove_widget(self.wid)
 
     def on_connect(self, obj, value):
         '''check if nodemcu is connected, and toggle Counter widget'''
@@ -90,12 +68,12 @@ class DashApp(App):
         else:
             animation = Animation(font_size =90, t='in_out_sine', duration= .5)
             Clock.schedule_once(self.remove_counter,.5)
-        animation.start(self.root.children[0].ids.time)
+        animation.start(self.root.children[0].ids["time"])
 
     def update_time(self, nap):
         '''update clock current time'''
         now = datetime.now() # get current time as datetime object
-        self.root.children[0].ids.time.text = now.strftime('%H:%M:%S')
+        self.timenow = now.strftime('%H:%M:%S')
 
     def serial_read(self,dt):
         '''connection, state and time change is handled here'''
@@ -105,20 +83,19 @@ class DashApp(App):
         if res == 'f':
             self.connect = False
         else: # connected
-            if self.connect == False: # not showing
-                self.connect = not self.connect # add widget
-            # check time left
-            self.timeleft = int(tl, base=16)
+            self.connect = True # add widget if False
+            # change time left
+            self.timeleft = str(int(tl, base=16))
             # handle state and trigger on_state
             if res == '1':
-                self.state = 'red'
+                self.state = COLOR['red']
             elif res == '2':
-                self.state = 'green'
+                self.state = COLOR['green']
             elif res == '3':
-                self.state = 'yellow'
+                self.state = COLOR['yellow']
             elif res == '4':
-                self.state = 'standby'
-                self.wid.text = 'Waiting..'
+                self.state = COLOR['standby']
+                self.timeleft = 'Waiting..'
             else:
                 exit(-2)
                 pass
@@ -140,5 +117,4 @@ if __name__ == '__main__':
     LabelBase.register(name="Roboto",
         fn_regular = 'font/Roboto-Thin.ttf',
         fn_bold = 'font/Roboto-Medium.ttf')
-    # Window.clearcolor = get_color_from_hex('#808080')
     DashApp().run()
